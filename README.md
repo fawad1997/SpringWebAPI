63116f88c2c81305a20e53b795142dd6a3bd47c8
Fawad <fawad_12@outlook.com>
2020-04-14 15:58:31 +0500

    hello world

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # SpringWebAPI
+Create a package for controllers in src>main>java>[your pakage name]/controller
+
+Add a controller to display hello world message. **@RestController** makes it restfull controller and **@RequestMapping(value = "/hello")** defines URL mapping
---
+++ src/main/java/com/restfulspring/apiexample/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.restfulspring.apiexample.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping(value = "/hello")
+    public String sayHello(){
+        return "Hello World!";
+    }
+}
