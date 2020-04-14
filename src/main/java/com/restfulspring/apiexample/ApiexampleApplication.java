ee38d2323931446cb310ba963d825503ae73a6a4
Fawad <fawad_12@outlook.com>
2020-04-14 15:43:05 +0500

    Sprint web projected created using spring initializr

---
+++ src/main/java/com/restfulspring/apiexample/ApiexampleApplication.java
@@ -0,0 +1,13 @@
+package com.restfulspring.apiexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiexampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/restfulspring/apiexample/ApiexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restfulspring.apiexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiexampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
