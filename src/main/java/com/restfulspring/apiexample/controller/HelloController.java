8f14936efaffc3a8ac40be5402a47434f232dacc
Fawad <fawad_12@outlook.com>
2020-04-16 11:57:52 +0500

    customizing localhost:8080 message

--- src/main/java/com/restfulspring/apiexample/controller/HelloController.java
+++ src/main/java/com/restfulspring/apiexample/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @RequestMapping(value = "/hello")
+    @RequestMapping(value = "/")
     public String sayHello(){
         return "Hello World!";
     }
